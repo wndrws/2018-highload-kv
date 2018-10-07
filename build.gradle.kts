09c007483e49debca4d53390a0e8ebe458ea7749
WebSter <wws.dev@gmail.com>
2018-10-07 18:39:19 +0300

    Add logging dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,14 @@ repositories {
     jcenter()
 }
 
+
+val loggingDependencies = listOf(
+        "org.slf4j:slf4j-api:1.7.25",
+        "org.apache.logging.log4j:log4j-api:2.11.1",
+        "org.apache.logging.log4j:log4j-1.2-api:2.11.1",
+        "org.apache.logging.log4j:log4j-slf4j-impl:2.11.1",
+        "org.apache.logging.log4j:log4j-core:2.11.1")
+
 dependencies {
     // Annotations for better code documentation
     compile("com.intellij:annotations:12.0")
@@ -30,6 +38,9 @@ dependencies {
 
     // one-nio tools
     compile("ru.odnoklassniki:one-nio:1.0.2")
+
+    // Logging
+    loggingDependencies.map { compile(it) }
 
     // JUnit Jupiter test framework
     testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
