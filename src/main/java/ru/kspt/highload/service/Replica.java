989d2d11bd9cf3defc57d4397d53c28d2804a85e
WebSter <wws.dev@gmail.com>
2018-12-15 18:48:13 +0300

    Add Replica class representing a remote node from the cluster

---
+++ src/main/java/ru/kspt/highload/service/Replica.java
@@ -0,0 +1,36 @@
+package ru.kspt.highload.service;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import one.nio.http.HttpClient;
+import one.nio.net.ConnectionString;
+
+import java.time.Duration;
+
+@ToString(exclude = "httpClient")
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "httpClient")
+public class Replica {
+    private static final String URL_SCHEMA = "http://";
+
+    private static final Duration TIMEOUT = Duration.ofSeconds(30);
+
+    final String host;
+
+    final int port;
+
+    @Getter(lazy = true)
+    private final HttpClient httpClient = createHttpClient();
+
+    private HttpClient createHttpClient() {
+        return new HttpClient(new ConnectionString(
+                URL_SCHEMA + host + ":" + port + "?timeout=" + TIMEOUT.toMillis()));
+    }
+
+    static Replica create(final String hostport) {
+        final String[] parts = hostport.split(":");
+        return new Replica(parts[0], Integer.valueOf(parts[1]));
+    }
+}
